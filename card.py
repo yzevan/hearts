35587b62b4c16733291d77367bdd991e23e1eb7d
Wanjun Li <lwj920121@gmail.com>
2018-08-12 09:31:00 +0800

    remove orderedenum

--- card.py
+++ card.py
@@ -5,8 +5,30 @@ most notably the type Card.
 
 import sys
 from random import shuffle
+from enum import Enum
 
-from orderedenum import OrderedEnum
+
+class OrderedEnum(Enum):
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        return NotImplemented
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        return NotImplemented
 
 
 class Suit(OrderedEnum):
